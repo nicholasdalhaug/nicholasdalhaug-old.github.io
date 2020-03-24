60c392ce9f20eab838214c27be6204014b8ed6fc
Nicholas Dalhaug <nicholasdalhaug@gmail.com>
2020-03-24 20:51:08 +0100

    Arbitrary commit before refactor

--- js/ui.js
+++ js/ui.js
@@ -7,6 +7,10 @@ document.addEventListener('DOMContentLoaded', function() {
   const exercise_form = document.querySelectorAll('#cf-exercise-form');
   M.Sidenav.init(exercise_form, {edge: 'right'});
   
+  // routine form sidenav
+  const routine_form = document.querySelectorAll('#cf-routine-form');
+  M.Sidenav.init(routine_form, {edge: 'right'});
+  
   // selects
   var selects = document.querySelectorAll('select');
   M.FormSelect.init(selects, {});
